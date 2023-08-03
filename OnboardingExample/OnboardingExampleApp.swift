46c78607333b5e522fd4116975d5e2661f434a2d
songmoro <97587585+songmoro@users.noreply.github.com>
2023-08-04 03:20:27 +0900

    Initial Commit

---
+++ OnboardingExample/OnboardingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingExampleApp.swift
+//  OnboardingExample
+//
+//  Created by 송재훈 on 2023/08/04.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
